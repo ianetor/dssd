b7d7ac210a2c7cb47b11fc47cf6b9522398c5565
tobias.pena <tobiaspena00@gmail.com>
2026-09-23 11:21:04 -0300

    frontend/

--- frontend/src/app/services/bonita.service.ts
+++ frontend/src/app/services/bonita.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../helpers/environment';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class BonitaService {
